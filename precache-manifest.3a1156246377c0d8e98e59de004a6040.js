937785425679d8b83d4f127c080ed230fdc68e3f
zixiz <gshachargilad@gmail.com>
2020-09-06 16:22:06 +0300

    Updates

---
+++ precache-manifest.3a1156246377c0d8e98e59de004a6040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773d13119b57607a7c149570aaca7d1e",
+    "url": "/israel-whatsup-web-link-builder-by-phone-number/index.html"
+  },
+  {
+    "revision": "6c1e38ad4b02961535bc",
+    "url": "/israel-whatsup-web-link-builder-by-phone-number/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6c1e38ad4b02961535bc",
+    "url": "/israel-whatsup-web-link-builder-by-phone-number/static/js/2.12cd2648.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/israel-whatsup-web-link-builder-by-phone-number/static/js/2.12cd2648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b02f0e0ce1c3ba738d9",
+    "url": "/israel-whatsup-web-link-builder-by-phone-number/static/js/main.d0042953.chunk.js"
+  },
+  {
+    "revision": "1f503a862f7b0278913e",
+    "url": "/israel-whatsup-web-link-builder-by-phone-number/static/js/runtime-main.3441965f.js"
+  }
+]);
